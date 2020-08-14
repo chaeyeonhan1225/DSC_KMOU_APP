9f1f6f3c0e95d4b3b51440b426c75bdb139fb066
chaeyeonhan1225 <gkscodus11@gmail.com>
2020-08-14 23:46:52 +0900

    FutureBuilder 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import './widgets/TopContainer.dart';
 import './widgets/BusCard.dart';
 import './widgets/BusInfo.dart';
@@ -8,12 +9,8 @@ import './BusPage.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    // statusBarColor: Colors.transparent,
-    // statusBarIconBrightness: Brightness.dark// transparent status bar
-    systemNavigationBarColor: Colors.white, // navigation bar color
-    statusBarColor: Colors.white, // status bar color
-    statusBarIconBrightness: Brightness.dark, // status bar icons' color
-    systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icons' color
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark// transparent status bar
   ));
   runApp(MyApp());
 }
@@ -42,6 +39,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _fetch1() async {
+    dynamic response = await http.get('http://192.168.75.191:3000/test');
+    // print(response);
+    return response.body.toString();
+  }
   @override
   Widget build(BuildContext context) {
     double rate = 1 / 375.0;
@@ -66,12 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Stack(
         children: <Widget>[
-
           TopContainer(
             child: Image.asset('images/BusPage/TopContainer.png'),
           ),
           SingleChildScrollView(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 80.0),
                 Text(
@@ -97,97 +99,124 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   textAlign: TextAlign.center,
                 ),
-                Container(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                FutureBuilder(
+                    future: _fetch1(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData == false) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text("Error");
+                      } else {
+                        return Column(
                           children: <Widget>[
-                            SizedBox(height: 43.0),
-                            BusCard(
-                                title: '셔틀 버스',
-                                width: 355 * fullWidth * rate,
-                                children: <Widget>[
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "해양대",
-                                  ),
-                                ]),
-                            SizedBox(height: 30.0),
-                            BusCard(
-                                title: '190번 버스',
-                                width: 355 * fullWidth * rate,
-                                children: <Widget>[
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "해양대",
-                                  ),
-                                  SizedBox(height: 14.0),
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "부산대",
-                                  ),
-                                  SizedBox(height: 14.0),
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "영도대교",
-                                  ),
-                                ]),
-                            SizedBox(height: 39.0),
-                            FlatButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => BusPage()));
-                              },
-                              child: Container(
-                                width: 355 * fullWidth * rate,
-                                height: 100,
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
+                            Container(
+                              child: Column(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: <Widget>[
-                                    Text(
-                                      "통근 버스 정보",
-                                      style: const TextStyle(
-                                        color: const Color(0xff131415),
-                                        fontWeight: FontWeight.w500,
-                                        fontFamily: "NotoSansKR",
-                                        fontStyle: FontStyle.normal,
-                                        fontSize: 28.0,
-                                      ),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          snapshot.data,
+                                          style: const TextStyle(
+                                            color: const Color(0xffffffff),
+                                            fontWeight: FontWeight.w300,
+                                            fontFamily: "NotoSansKR",
+                                            fontStyle: FontStyle.normal,
+                                            fontSize: 20.0,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                        SizedBox(height: 43.0),
+                                        BusCard(
+                                            title: '셔틀 버스',
+                                            width: 355 * fullWidth * rate,
+                                            children: <Widget>[
+                                              BusInfo(
+                                                width: 100 * fullWidth * rate,
+                                                title: "해양대",
+                                              ),
+                                            ]),
+                                        SizedBox(height: 30.0),
+                                        BusCard(
+                                            title: '190번 버스',
+                                            width: 355 * fullWidth * rate,
+                                            children: <Widget>[
+                                              BusInfo(
+                                                width: 100 * fullWidth * rate,
+                                                title: "해양대",
+                                              ),
+                                              SizedBox(height: 14.0),
+                                              BusInfo(
+                                                width: 100 * fullWidth * rate,
+                                                title: "부산대",
+                                              ),
+                                              SizedBox(height: 14.0),
+                                              BusInfo(
+                                                width: 100 * fullWidth * rate,
+                                                title: "영도대교",
+                                              ),
+                                            ]),
+                                        SizedBox(height: 39.0),
+                                        FlatButton(
+                                          onPressed: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) => BusPage()));
+                                          },
+                                          child: Container(
+                                            width: 355 * fullWidth * rate,
+                                            height: 100,
+                                            child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: <Widget>[
+                                                Text(
+                                                  "통근 버스 정보",
+                                                  style: const TextStyle(
+                                                    color: const Color(0xff131415),
+                                                    fontWeight: FontWeight.w500,
+                                                    fontFamily: "NotoSansKR",
+                                                    fontStyle: FontStyle.normal,
+                                                    fontSize: 28.0,
+                                                  ),
+                                                ),
+                                                SizedBox(width: 22 * fullWidth * rate),
+                                                SizedBox(
+                                                  width: 62,
+                                                  height: 44,
+                                                  child: Image.asset(
+                                                      "images/BusPage/commuterbus.png"),
+                                                )
+                                              ],
+                                            ),
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                              BorderRadius.all(Radius.circular(18)),
+                                              border: Border.all(
+                                                  color: const Color(0xff842fb5), width: 1),
+                                              boxShadow: [
+                                                BoxShadow(
+                                                    color: const Color(0x80cacaca),
+                                                    offset: Offset(0, -1),
+                                                    blurRadius: 16,
+                                                    spreadRadius: 2)
+                                              ],
+                                              color: const Color(0xffffffff),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    SizedBox(width: 22 * fullWidth * rate),
-                                    SizedBox(
-                                      width: 62,
-                                      height: 44,
-                                      child: Image.asset(
-                                          "images/BusPage/commuterbus.png"),
-                                    )
-                                  ],
-                                ),
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(18)),
-                                  border: Border.all(
-                                      color: const Color(0xff842fb5), width: 1),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: const Color(0x80cacaca),
-                                        offset: Offset(0, -1),
-                                        blurRadius: 16,
-                                        spreadRadius: 2)
-                                  ],
-                                  color: const Color(0xffffffff),
-                                ),
-                              ),
+                                  ]),
                             ),
                           ],
-                        ),
-                      ]),
+                        );
+                      }
+                    }
                 ),
+
               ],
             ),
           )
