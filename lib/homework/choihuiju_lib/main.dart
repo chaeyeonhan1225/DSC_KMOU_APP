1a8148130c2f173f782ddfc1551653701f860b7c
choihuiju <cgw7412@gmail.com>
2020-08-25 21:40:01 +0900

    homework 완료_choihuiju

--- lib/homework/choihuiju_lib/main.dart
+++ lib/homework/choihuiju_lib/main.dart
@@ -32,49 +32,49 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        alignment: Alignment.centerLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Spacer(flex: 1,
-            ),
-            SizedBox(
-              child: Text("학교버스 셔틀",
-              style: TextStyle(color: Colors.black, fontSize: 30),
-            ),
-              height: 200,
-              width: 200,
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            Text("학교↔남포동간 1회 운행",
-              style: TextStyle(color: Colors.purple, fontSize: 20),
-            ),
-            Spacer(
-              flex: 1,
-            ),
+          alignment: Alignment.centerLeft,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Spacer(flex: 1,
+              ),
+              SizedBox(
+                child: Text("학교버스 셔틀",
+                  style: TextStyle(color: Colors.black, fontSize: 30),
+                ),
+                height: 200,
+                width: 200,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Text("학교↔남포동간 1회 운행",
+                style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+              Spacer(
+                flex: 1,
+              ),
 
-            Text("학교출발(09:00 > 영도대교 대궁한정식 앞(09:25) >  학교도착(09:50))",
-              style: TextStyle(color: Colors.black38, fontSize: 16),
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            Text("학교↔대연동간 1회 운행", style: TextStyle(color: Colors.purple, fontSize: 20),
-            ),
-            Spacer(
-              flex: 1,
-            ),
-            Text("학교출발(08:50) > 경성대부경대역 3번출구 눈사랑안경점 앞(09:20) > 학교도착(09:55))",
-              style: TextStyle(color: Colors.black38, fontSize: 16),
-            ),
-            Spacer(
-              flex: 1,
-            ),
-          ],
-        )
+              Text("학교출발(09:00 > 영도대교 대궁한정식 앞(09:25) >  학교도착(09:50))",
+                style: TextStyle(color: Colors.black38, fontSize: 16),
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Text("학교↔대연동간 1회 운행", style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Text("학교출발(08:50) > 경성대부경대역 3번출구 눈사랑안경점 앞(09:20) > 학교도착(09:55))",
+                style: TextStyle(color: Colors.black38, fontSize: 16),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+            ],
+          )
       ),
     );
   }
